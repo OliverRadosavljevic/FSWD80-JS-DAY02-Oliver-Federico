5d3c174281d4252bcd1e888d32eb2d2113ec0a16
Oliver Radosavljevic <oliver.radosavljevic@hotmail.com>
2019-09-23 10:49:57 +0200

    added basic

---
+++ Oliver/Basic/script.js
@@ -0,0 +1,43 @@
+//Basic 1
+// function crystalGazer(numChild,partName,geoLoc,jobTitle){
+// document.write("You will be a " + jobTitle + " in " + geoLoc + " and married to " + partName + " with " + numChild + "children");
+// }
+// crystalGazer(5,"Christina","Austria","Programmer");
+
+//Basic 2
+// function ageCalculator(birthYear, currentYear){
+// document.write("You are either " + (currentYear-birthYear) + " or "+ ((currentYear-birthYear)-1));
+// }
+// ageCalculator(2001,2019)
+
+//Basic 3
+// function ageCalculator(Year){
+// document.write("You are either " + (Year) + " or "+ (Year-1));
+// }
+// ageCalculator(2019-2001)
+
+//Basic 4
+// function degCon(deg){
+// 	document.write(deg + " Grad sind " + (deg  * (Math.PI/180)) + " Radianten");
+// }
+// degCon(90);
+
+//Basic 5
+// function calcBox(width, height, depth){
+// 	var area = width * height;
+// 	var volume = width * height * depth;
+
+// 	document.write("The area is " + area + " and the volume is " + volume);
+// }
+
+// calcBox(35,15,55);
+
+//Basic 6
+// try{
+// var person = prompt("Please enter your name", "Harry Potter");
+
+// document.getElementById("message").innerHTML = "Hello " + person + "! How are you today?";
+// }
+// catch(err){
+// 	console.log(err.message);
+// }
