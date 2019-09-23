99dac5c5f63f211727bf4d6aeddabdd157c407ed
Oliver Radosavljevic <oliver.radosavljevic@hotmail.com>
2019-09-23 15:46:53 +0200

    added advanced

---
+++ Oliver/Advanced/script.js
@@ -0,0 +1,61 @@
+//Advanced 1
+// function timeCalc(time){
+// 	var hours = time/60;
+// 	var minutes = time%60;
+	
+// 	document.write(Math.round(hours)+" hours and " + minutes + " minutes");
+// }
+// timeCalc(210);
+
+
+//Advanced 2
+// var date = function(){
+// 	var day = new Date().getDate()-1;
+// 	var month;
+// 	var year = new Date().getFullYear();
+// 	switch(new Date().getMonth()){
+// 		case 0:
+// 		month="Jan";
+// 		break;
+// 		case 1:
+// 		month="Feb";
+// 		break;
+// 		case 2:
+// 		month="Mar";
+// 		break;
+// 		case 3:
+// 		month="Apr";
+// 		break;
+// 		case 4:
+// 		month="May";
+// 		break;
+// 		case 5:
+// 		month="June";
+// 		break;
+// 		case 6:
+// 		month="July";
+// 		break;
+// 		case 7:
+// 		month="Aug";
+// 		break;
+// 		case 8:
+// 		month="Sep";
+// 		break;
+// 		case 9:
+// 		month="Oct";
+// 		break;
+// 		case 10:
+// 		month="Nov";
+// 		break;
+// 		case 11:
+// 		month="Dec";
+// 		break;
+// 	}
+// 	document.write("Yesterday was the " + month + " " + day + ", " +year);
+// }
+
+// date();
+
+
+
+
