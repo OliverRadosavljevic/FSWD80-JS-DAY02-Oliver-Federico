32dcd5708b90a482a80b415dfb36142b686d547c
Oliver Radosavljevic <oliver.radosavljevic@hotmail.com>
2019-09-23 13:30:55 +0200

    finished intermediate

---
+++ Oliver/Intermediate/script.js
@@ -0,0 +1,52 @@
+//Intermediate 1
+// var cap = function(a){
+// 	var b = a.charAt(0).toUpperCase() + a.substring(1);
+// 	document.write(b);
+// }
+
+// cap("i am a web developer");
+
+//Intermediate 2
+// var a =[1,2,3,4,5];
+// function check(num){
+// 	var error = false;
+// 	for(var i = 0; i<a.length;i++){
+// 		if(num == a[i]){
+// 			var found = true;
+// 		}
+// 	}
+// 	if(!found){
+// 		error = true;
+// 	}
+// 	return error;
+// }
+// var error = false;
+// var math = prompt("Write your grade from Math: ");
+// var matha = parseInt(math);
+// if(matha > 5){
+// 	matha = 5;
+// }else if(matha < 1){
+// 	matha = 1;
+// }
+// var physic = prompt("Write your grade from Physics: ")
+// var physica = parseInt(physic);
+// if(physica > 5){
+// 	physica = 5;
+// }else if(physica < 1){
+// 	physica = 1;
+// }
+// var geo = prompt("Write your grade from geography: ")
+// var geoa = parseInt(geo);
+// if(geoa > 5){
+// 	geoa = 5;
+// }else if(geoa < 1){
+// 	geoa = 1;
+// }
+
+// if(error){
+// 	alert("false input");
+// }else{
+// 	var sum = matha + physica +geoa;
+// 	var avg = sum/3;
+// 	document.write("The sum is " + sum + " and the average is " + avg);
+// }
