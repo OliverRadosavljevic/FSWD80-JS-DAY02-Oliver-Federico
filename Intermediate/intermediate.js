1f19a31ae9ca3ea83d7b3e7a49a0f1e8cfb4ec94
defaziofederico <defaz86@gmail.com>
2019-09-23 13:29:42 +0200

    intermediate

---
+++ Intermediate/intermediate.js
@@ -0,0 +1,18 @@
+//Intermediate1
+// var aaa = function(a)  {
+// var res = a.charAt(0).toUpperCase() + a.substring(1);
+// document.write(res); 
+// }
+
+// aaa("i am a Javascript developer");
+
+
+// function avg (a,b,c) {
+ 
+//    var sum=(a+b+c);
+//    var avge=(sum/3);
+//    document.write(sum+"<br>")
+//    document.write(avge)
+// }
+
+// avg (6,8,4)
